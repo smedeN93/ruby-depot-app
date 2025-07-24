98ed71abae3d2384862472e9e79146236cc7f1af
smedeN93 <nicklasmc93@gmail.com>
2025-07-24 15:34:50 +0200

    Add cart func

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_21_162856) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_24_133227) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
